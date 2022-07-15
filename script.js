17ef5c4d107da8447049732c74c3dff5a06c19c7
actiniumn404 <83673744+actiniumn404@users.noreply.github.com>
2022-07-15 13:49:32 -0700

    More features

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const generate_puzzle = (size) => {
             $("#puzzle tr:last-of-type").append("<td class='square'><input maxlength='1'></td>")
         }
     }
+    $(".square").css("--size", size)
 }
 
 const get_puzzle = () => {
@@ -28,17 +29,44 @@ $("#process").click(async () => {
 
     let res = await fetch(`/api/solve?size=${$("#puzzle_size").val()}&grid=${get_puzzle()}`)
     res = await res.json()
-    console.log(res)
 
     $("#results").html("")
-    for (size in res){
-        $("#results").append(`<h4>${size} letters</h4><ul></ul>`)
-        for (word of res[size]){
-            $("#results ul:last-of-type").append(`<li>${word}</li>`)
-        }
+    btn.prop("disabled", false)
+
+    if (res["error"]){
+        return $("#results").html(`<strong style="color: var(--theme)">ERROR: ${res["error"]}</strong>`)
     }
 
-    btn.prop("disabled", false)
+    for (size in res){
+        let width = 70
+        let word_list = [...new Set(res[size])]
+        word_list.sort()
+
+
+        $("#results").append(`<h4>${size} letters</h4><ul></ul>`)
+        for (word of word_list){
+            $("#results ul:last-of-type").append(`<li style="width: fit-content;width: -moz-fit-content;">${word}</li>`)
+            width = Math.max(width, $("#results ul:last-of-type li:last-of-type").width())
+        }
+        $("#results ul:last-of-type li").css("width", "initial")
+        $("#results ul:last-of-type").css("grid-template-columns", `repeat(auto-fill, minmax(${Math.ceil(width) + 10}px, 1fr))`)
+    }
+
+})
+
+$("#exportDscd").click(() => {
+    let index = 0;
+    let res = ""
+    for (let letter of get_puzzle()){
+        res += `:squaredle${letter.toUpperCase()}: `
+        if (index % 4 === 0){
+            res += "\n"
+        }
+        index += 1
+    }
+
+    navigation.clipboard.writeText(res)
+    alert("Copied data to clipboard!")
 })
 
 
