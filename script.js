4953686f788f4984f444fb2ab4322d24e4b0e171
actiniumn404 <83673744+actiniumn404@users.noreply.github.com>
2022-07-16 21:42:36 -0700

    Awkward Word detector

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let num_words = 0
 
 const generate_puzzle = (size) => {
+    $("#puzzle_size").val(size)
     let puzzle = $("#puzzle")
     puzzle.html("")
     for (let i = 0; i < size; i++){
@@ -19,6 +20,14 @@ const get_puzzle = () => {
     })
 
     return res
+}
+
+const load_puzzle = (puzzle) => {
+    generate_puzzle(Math.sqrt(puzzle.length))
+    let squares = $(".square");
+    for (let i = 0; i < squares.length; i++){
+        squares[i].children[0].value = puzzle[i]
+    }
 }
 
 $("#puzzle_size").click(() => {
@@ -41,6 +50,7 @@ $("#process").click(async () => {
     }
 
     num_words = 0
+    let num_awkward = 0;
 
     for (size in res){
         let width = 70
@@ -51,14 +61,18 @@ $("#process").click(async () => {
         $("#results").append(`<h4>${size} letters</h4><ul></ul>`)
         for (word of word_list){
             num_words += 1
-            $("#results ul:last-of-type").append(`<li style="width: fit-content;width: -moz-fit-content;">${word}</li>`)
+            if (awkward_words.has(word)){
+                num_awkward += 1;
+            }
+            $("#results ul:last-of-type").append(`<li style="width: fit-content;width: -moz-fit-content;${awkward_words.has(word) ? "color: mediumpurple": ""}">${word}</li>`)
             width = Math.max(width, $("#results ul:last-of-type li:last-of-type").width())
         }
         $("#results ul:last-of-type li").css("width", "initial")
         $("#results ul:last-of-type").css("grid-template-columns", `repeat(auto-fill, minmax(${Math.ceil(width) + 10}px, 1fr))`)
     }
 
-    $("#time_numwords").html(`${num_words} result${num_words !== 1 ? "s": ""} in ${((Date.now() - start) / 1000).toFixed(2)} seconds`)
+    $("#time_numwords").html(`${num_words} result${num_words !== 1 ? "s": ""} in ${((Date.now() - start) / 1000).toFixed(2)} seconds<br>
+    ${num_awkward} awkward word${num_awkward !== 1 ? "s": ""}`)
 
 })
 
